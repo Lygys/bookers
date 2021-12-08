71bd03a01201377049f987bcafcd16601771fa05
EC2 Default User <ec2-user@ip-172-31-35-179.ec2.internal>
2021-12-08 09:57:13 +0000

    テスト終了。オールグリーン！

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2021_12_07_063646) do
 
-  create_table "bookers", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "body"
